5811831d8ff8f31818f4961accc85377c2a80703
jinsoo <admin@binary01.me>
2022-06-22 22:31:23 +0900

    feat: 공부즁..

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Redirect, Req } from '@nestjs/common';
+import { Request } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,19 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/test')
+  getHello2(@Req() req: Request): string {
+    console.log(req);
+    return this.appService.getHello();
+  }
+
+  @Get('redirect/docs')
+  @Redirect('https://docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version && version === '5') {
+      return { url: 'https://docs.nestjs.com/v5/' };
+    }
   }
 }
