dad2b9e0dcbb00d4a11be4ce33290fb70a168e51
jinsoo <admin@binary01.me>
2022-06-22 22:44:47 +0900

    clear

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { HealthCheckModule } from './health-check/health-check.module';
-import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [HealthCheckModule, UsersModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
